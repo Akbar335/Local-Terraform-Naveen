ed47f6ffaba06f14d6d42beeb8e8a37568269074
Akbar335 <62642055+Akbar335@users.noreply.github.com>
2025-08-21 21:50:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ terraform {
 
 provider "aws" {
   region     = var.aws_region
-  access_key = "AKIA3WUYYIPHQ3MI4ZL2"
-  secret_key = "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"
+  access_key = "AKIA3WUYYIPH4OKBNIVM"
+  secret_key = "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"
 }
